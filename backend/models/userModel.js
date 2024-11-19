ac524e1f3c01fd6287d8d8daca15bde0fa330821
amit murmu <your.akmurmu6580@google.com>
2024-11-19 08:46:44 +0530

    removed password from userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
     email: { type: String },
     profilePic: { type: String, default: "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png" },
     relation: { type: String, required: true, enum: ["Sibling", "Friend", "Cousine", "Uncle", "Aunt", "Niece", "Nephew", "Unknown", "Admin"], default: "Friend" },
